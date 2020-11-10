2a3f28f0dfc7dea832a7c73437765c9f3b0b68a4
Florie Anstett <florie.anstett@gmail.com>
2020-11-10 16:35:45 +0100

    ajout condition pour gérer la quantité des produits dans le panier

--- src/assets/components/Panier.js
+++ src/assets/components/Panier.js
@@ -1,30 +1,43 @@
 import React from "react";
 
 const Panier = ({ cart, setCard }) => {
-  console.log(cart);
+  // console.log(cart);
   return (
     <section className="basket">
       <button className={cart.length !== 0 ? "fullcart" : "emptycart"}>
         Valider mon panier
       </button>
       {cart.length === 0 ? (
-        <div>Votre panier est vide</div>
+        <div className="empty-text">Votre panier est vide</div>
       ) : (
-        <section>
-          {cart.map((item, i) => {
-            return (
-              <>
-                <div key={item.id} className="command">
-                  <span></span>
-                  <span>{item.title}</span>
+        cart.map((item, i) => {
+          return (
+            <section key={item.id}>
+              <div className="command">
+                <span></span>
+                <span>{item.title}</span>
+                <span>{item.price.replace(".", ",")} €</span>
+              </div>
+              <hr />
+              <div className="detail">
+                <section>
+                  <span>Sous-total</span>
                   <span>{item.price.replace(".", ",")} €</span>
-                </div>
-                <div></div>
-                <div></div>
-              </>
-            );
-          })}
-        </section>
+                </section>
+                <section>
+                  <span>Frais de livraison</span>
+                  <span>2,50€ </span>
+                </section>
+                <section>
+                  <span>Total</span>
+                  <span>{item.price} + 2.50</span>
+                </section>
+              </div>
+              <hr />
+              <div></div>
+            </section>
+          );
+        })
       )}
     </section>
   );
--- src/assets/components/Section.js
+++ src/assets/components/Section.js
@@ -3,31 +3,44 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Section = ({ data, categorie, i, cart, setCart }) => {
+  //console.log(cart[0].title);
   return (
     <>
       {categorie.meals.map((meal, index) => {
         return (
           <article
-            key={meal.id}
+            key={index}
             className="section"
             onClick={() => {
               const tab = [...cart];
-              tab.push(meal);
-              setCart(tab);
+              for (let i = 0; i <= tab.length; i++) {
+                if (tab[i].title === meal.title) {
+                  alert("déjà présent");
+                } else {
+                  alert("nouveau produit");
+                }
+              }
+              // tab.push({
+              //   id: meal.id,
+              //   title: meal.title,
+              //   price: meal.price,
+              //   quantity: 1,
+              // });
+              // setCart(tab);
             }}
           >
             <div>
               <h3>{meal.title}</h3>
               <p>{meal.description}</p>
               <span>{meal.price.replace(".", ",")} €</span>
-              <span className={!meal.popular ? "hidden" : "popular"}>
+              <span className={`popular ${!meal.popular && "hidden"}`}>
                 <i>
                   <FontAwesomeIcon icon="star" />
                 </i>
                 populaire
               </span>
             </div>
-            <div className={!meal.picture ? "hidden" : "display"}>
+            <div className={!meal.popular ? "hidden" : "popular"}>
               <img src={meal.picture} alt="" />
             </div>
           </article>
